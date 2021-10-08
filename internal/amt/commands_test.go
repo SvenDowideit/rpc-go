6727c7a8c80fcd8b089c1c6df584d142846858f5
Craig-Spencer-12 <craigspencer.linux@gmail.com>
2021-10-07 22:25:31 -0700

    final tweaks

--- internal/amt/commands_test.go
+++ internal/amt/commands_test.go
@@ -40,57 +40,48 @@ func init() {
 
 // Tests
 func TestGetGUID(t *testing.T) {
-	// amt = Command{}
-	// amt.PTHI = MockPTHICommands{}
 	result, err := amt.GetUUID()
 	assert.NoError(t, err)
 	assert.NotEmpty(t, result)
 }
 
 func TestGetControlmode(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetControlMode()
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetDNSSuffix(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetDNSSuffix()
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetCertificateHashes(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetCertificateHashes()
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetRemoteAccessConnectionStatus(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetRemoteAccessConnectionStatus()
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetLANInterfaceSettingsTrue(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetLANInterfaceSettings(true)
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetLANInterfaceSettingsFalse(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetLANInterfaceSettings(false)
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
 }
 
 func TestGetLocalSystemAccount(t *testing.T) {
-	amt := Command{}
 	result, err := amt.GetLocalSystemAccount()
 	assert.NoError(t, err)
 	assert.NotEqual(t, -1, result)
