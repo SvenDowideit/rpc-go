a51c433bd77cf7496547ea4d3d19bd4d07327ec9
Craig-Spencer-12 <craigspencer.linux@gmail.com>
2021-10-14 20:11:28 -0700

    more mocking

--- internal/amt/commands_test.go
+++ internal/amt/commands_test.go
@@ -16,32 +16,33 @@ type MockPTHICommands struct {}
 func (c MockPTHICommands) Open() error { return nil }
 func (c MockPTHICommands) Close() {}
 func (c MockPTHICommands) Call(command []byte, commandSize uint32) (result []byte, err error) {return nil, nil}
-func (c MockPTHICommands) GetCodeVersions() (pthi.GetCodeVersionsResponse, error) {return pthi.GetCodeVersionsResponse{}, nil}
+func (c MockPTHICommands) GetCodeVersions() (pthi.GetCodeVersionsResponse, error) {
+	return pthi.GetCodeVersionsResponse{
+		CodeVersion: pthi.CodeVersions{
+			BiosVersion: [65]uint8{84, 101, 115, 116},
+			VersionsCount: 0,
+			Versions: [50]pthi.AMTVersionType{},
+		},
+	}, nil
+}
 func (c MockPTHICommands) GetUUID() (uuid string, err error) { return "\xd2?\x11\x1c%3\x94E\xa2rT\xb2\x03\x8b\xeb\a" , nil }
 func (c MockPTHICommands) GetControlMode() (state int, err error) {return 0, nil}
-func (c MockPTHICommands) GetDNSSuffix() (suffix string, err error) {return "test", nil}
+func (c MockPTHICommands) GetDNSSuffix() (suffix string, err error) {return "Test", nil}
 func (c MockPTHICommands) GetCertificateHashes() (hashEntryList []pthi.CertHashEntry, err error) {
-	var buf [1000]uint8
-	buf[0] = 'A'
-	hash := []pthi.CertHashEntry{}
-	hash = append(hash, pthi.CertHashEntry{
-		CertificateHash: [64]uint8{231,104,86,52,239,172,246,154,206,147,154,107,37,91,123,79,171,239,66,147,91,80,162,101,172,181,203,96,39,228,78,112,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		Name: pthi.AMTANSIString{Length: 1, Buffer: buf},
-		HashAlgorithm: 2,
-		IsActive: 1,
-		IsDefault: 1,
-	})
-	return hash, nil
+	return []pthi.CertHashEntry{{
+		CertificateHash: [64]uint8{84, 101, 115, 116},
+		Name:            pthi.AMTANSIString{Length: 4, Buffer: [1000]uint8{84, 101, 115, 116}},
+		HashAlgorithm:   2,
+		IsActive:        1,
+		IsDefault:       1,
+	}}, nil
 }
 func (c MockPTHICommands) GetRemoteAccessConnectionStatus() (RAStatus pthi.GetRemoteAccessConnectionStatusResponse, err error) {
-	var buf [1000]uint8
-	buf[0] = 'A'
 	return pthi.GetRemoteAccessConnectionStatusResponse{
 		NetworkStatus: 2,
 		RemoteStatus: 0,
 		RemoteTrigger: 0,
-		MPSHostname: pthi.AMTANSIString{Length: 1, Buffer: buf},
-
+		MPSHostname: pthi.AMTANSIString{Length: 4, Buffer: [1000]uint8{84, 101, 115, 116}},
 	}, nil
 }
 func (c MockPTHICommands) GetLANInterfaceSettings(useWireless bool) (LANInterface pthi.GetLANInterfaceSettingsResponse, err error) {
@@ -54,7 +55,6 @@ func (c MockPTHICommands) GetLANInterfaceSettings(useWireless bool) (LANInterfac
 			LinkStatus: 0,
 			MacAddress: [6]uint8{0,0,0,0,0,0},
 		}, nil
-
 	} else {
 		return pthi.GetLANInterfaceSettingsResponse{
 			Enabled: 1,
@@ -67,7 +67,13 @@ func (c MockPTHICommands) GetLANInterfaceSettings(useWireless bool) (LANInterfac
 	}
 }
 func (c MockPTHICommands) GetLocalSystemAccount() (localAccount pthi.GetLocalSystemAccountResponse, err error) {
-	return pthi.GetLocalSystemAccountResponse{}, nil
+	return pthi.GetLocalSystemAccountResponse{
+		Status: 0,
+		Account: pthi.LocalSystemAccount{
+			Username: [33]uint8{84, 101, 115, 116},
+			Password: [33]uint8{84, 101, 115, 116},
+		},
+	}, nil
 }
 
 var amt Command
